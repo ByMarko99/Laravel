bcbb90d1d00214ee2e80f378a5bbbda6b82fcc81
Marko Gros <markogrostx@gmail.com>
2023-11-13 13:36:40 +0100

    more stuff

--- eskola/app/Http/Controllers/CursoController.php
+++ eskola/app/Http/Controllers/CursoController.php
@@ -8,15 +8,20 @@ use Illuminate\Http\Request;
 class CursoController extends Controller
 {
     public function index(){
-        return "Ongi etorri a mis CURSOS desde el controlador";
+        // return "Ongi etorri a mis CURSOS desde el controlador";
+        return view('cursos.index');
     }
 
     public function create(){
-        return "Hemen ikastaro berri bat sortuko dugu desde el controlador";
+        // return "Hemen ikastaro berri bat sortuko dugu desde el controlador";
+        return view('cursos.create');
     }
 
     public function show($curso){
-        return "Hau da nire ikastaroa " . $curso. " -------- desde el controlador";
+        // return "Hau da nire ikastaroa " . $curso. " -------- desde el controlador";
+        return view('cursos.show', compact('curso')); // compact('curso') = ['curso' => $curso]
     }
+
+
 
 }
