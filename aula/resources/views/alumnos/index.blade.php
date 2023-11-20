75bbae00086c3d2f2631094289095b4f3ddfbd5c
Marko Gros <markogrostx@gmail.com>
2023-11-20 12:36:07 +0100

    Crud

--- aula/resources/views/alumnos/index.blade.php
+++ aula/resources/views/alumnos/index.blade.php
@@ -6,10 +6,19 @@
 @section('content')
     <h1>IKASLE ZERRENDA</h1>
     <ol>
-    @foreach ($alumnos as $alumno)
-     <li> <a href="{{route('alumnos.show', $alumno)}}"> {{$alumno->nombre_apellido}} - {{$alumno->edad}} </a> </li>
-   <a href="{{route('alumnos.edit', $alumno)}}">Editatu</a>
+        @foreach ($alumnos as $alumno)
+            <li> <a href="{{ route('alumnos.show', $alumno) }}"> {{ $alumno->nombre_apellido }} - {{ $alumno->edad }} </a>
+            </li>
+            <br>
+            <a href="{{ route('alumnos.edit', $alumno) }}"
+                style="color: #4A4A4A; text-decoration: none; background-color: #D9D9D9; padding: 10px 20px; border-radius: 5px; font-size: 14px;">Editatu</a>
+            <br>
+            <br>
+            <a href="{{ route('alumnos.delete', $alumno) }}"
+            style="color: #d42828; text-decoration: none; background-color: #D9D9D9; padding: 10px 20px; border-radius: 5px; font-size: 14px;">Ezabatu</a>
 
-    @endforeach
+        @endforeach
     </ol>
+    <br>
+
 @endsection
--- aula/routes/web.php
+++ aula/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 
 });
-
+/*
 Route::get('/', [AlumnoController::class, 'index']) -> name('alumnos.index');
 Route::get('alumnos', [AlumnoController::class, 'index']) -> name('alumnos.index');
 
@@ -29,6 +29,31 @@ Route::post('alumnos/store', [AlumnoController::class, 'store']) -> name('alumno
 Route::get('alumnos/edit/{alumno}', [AlumnoController::class, 'edit']) -> name('alumnos.edit');
 Route::put('alumnos/update/{alumno}', [AlumnoController::class, 'update']) -> name('alumnos.update');
 
-Route::get('alumnos/{alumno}', [AlumnoController::class, 'show']) -> name('alumnos.show');
+Route::delete('alumnos/{alumno}', [AlumnoController::class, 'destroy']) -> name('alumnos.destroy');
+Route::get('alumnos/delete/{alumno}', [AlumnoController::class, 'delete']) -> name('alumnos.delete');
+
+Route::get('alumnos/{alumno}', [AlumnoController::class, 'show']) -> name('alumnos.show'); */
+
+/* Route::controller(AlumnoController::class)->group(function (){
+
+
+    Route::get('/',  'index') -> name('alumnos.index');
+    Route::get('alumnos','index') -> name('alumnos.index');
+
+    Route::get('alumnos/create', 'create') -> name('alumnos.create');
+    Route::post('alumnos/store', 'store') -> name('alumnos.store');
+
+    Route::get('alumnos/edit/{alumno}', 'edit') -> name('alumnos.edit');
+    Route::put('alumnos/update/{alumno}','update') -> name('alumnos.update');
+
+    Route::delete('alumnos/{alumno}', 'destroy') -> name('alumnos.destroy');
+    Route::get('alumnos/delete/{alumno}', 'delete') -> name('alumnos.delete');
+
+    Route::get('alumnos/{alumno}', 'show') -> name('alumnos.show');
+});
+ */
+Route::get('alumnos/delete/{alumno}', [AlumnoController::class, 'delete']) -> name('alumnos.delete');
+
+Route::resource('alumnos', AlumnoController::class);
 
 ?>
