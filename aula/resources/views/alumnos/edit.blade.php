eb8936ce53d7574bd30a729d52b9f8882706a3a4
Marko Gros <markogrostx@gmail.com>
2023-11-20 14:01:14 +0100

    more validations

--- aula/resources/views/alumnos/edit.blade.php
+++ aula/resources/views/alumnos/edit.blade.php
@@ -9,13 +9,25 @@
         @csrf
         @method('put')
         <label for="nombre">Nombre y Apellido</label>
-        <input type="text" name="nombre" value="{{$alumno->nombre_apellido}}"><br>
+        <input type="text" name="nombre" value="{{old('nombre', $alumno->nombre_apellido)}}"><br>
+        @error('nombre')
+        <p class="akatsa"><strong>{{$message}}</strong></p>
+     @enderror
         <label for="edad">Edad</label>
-        <input type="text" name="edad" value="{{$alumno->edad}}" ><br>
+        <input type="text" name="edad" value="{{old('edad', $alumno->edad)}}" ><br>
+        @error('edad')
+        <p class="akatsa"><strong>{{$message}}</strong></p>
+     @enderror
         <label for="telefono">Telefono</label>
-        <input type="text" name="telefono" value="{{$alumno->telefono}}" ><br>
+        <input type="text" name="telefono" value="{{old('telefono', $alumno->telefono)}}" ><br>
+        @error('telefono')
+        <p class="akatsa"><strong>{{$message}}</strong></p>
+     @enderror
         <label for="direccion">Direccion</label>
-        <input type="text" name="direccion" value="{{$alumno->direccion}}" ><br>
+        <input type="text" name="direccion" value="{{old('direccion', $alumno->direccion)}}" ><br>
+        @error('direccion')
+        <p class="akatsa"><strong>{{$message}}</strong></p>
+     @enderror
         <input type="submit" value="Bidali">
     </form>
 @endsection
