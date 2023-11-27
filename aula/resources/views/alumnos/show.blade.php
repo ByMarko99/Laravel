1bf3915ca255de3c37cbc3a826b4437d3d11f341
Marko Gros <markogrostx@gmail.com>
2023-11-27 14:17:25 +0100

    foto

--- aula/resources/views/alumnos/show.blade.php
+++ aula/resources/views/alumnos/show.blade.php
@@ -4,13 +4,20 @@
 
 @section('content')
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
-        <h2 style="margin-bottom: 20px;">IKASLE: {{$alumno->nombre_apellido}}</h2>
         <ul style="list-style-type: none; padding: 0;">
-            <li style="margin-bottom: 10px;"> Izena: {{$alumno->nombre_apellido}} </li>
-            <li style="margin-bottom: 10px;"> Adina: {{$alumno->edad}} </li>
-            <li style="margin-bottom: 10px;"> Telefonoa: {{$alumno->telefono}} </li>
-            <li style="margin-bottom: 20px;"> Direcciona: {{$alumno->direccion}} </li>
-            <a href="{{route('alumnos.edit', $alumno)}}" style="background-color: #04AA6D; color: white; padding: 10px 20px; text-decoration: none; border-radius: 4px;">Editatu</a>
+            <h2 style="margin-bottom: 20px;">IKASLE: {{ $alumno->nombre_apellido }}</h2>
+
+            @if($alumno->foto)
+            <img src="{{ url('alumnos/'.$alumno->foto) }}" alt="" style="width: 200px; height: 200px; margin-top: 20px;">
+            @else
+            <img src="{{ url('storage/alumnos/default.jpg') }}" alt="" style="width: 200px; height: 200px; margin-top: 20px;">
+@endif
+            <li style="margin-bottom: 10px;"> Izena: {{ $alumno->nombre_apellido }} </li>
+            <li style="margin-bottom: 10px;"> Adina: {{ $alumno->edad }} </li>
+            <li style="margin-bottom: 10px;"> Telefonoa: {{ $alumno->telefono }} </li>
+            <li style="margin-bottom: 20px;"> Direcciona: {{ $alumno->direccion }} </li>
+            <a href="{{ route('alumnos.edit', $alumno) }}"
+                style="background-color: #04AA6D; color: white; padding: 10px 20px; text-decoration: none; border-radius: 4px;">Editatu</a>
         </ul>
     </div>
 @endsection
