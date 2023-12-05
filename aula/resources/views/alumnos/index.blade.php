c976b8e7b3e37c6d25dc5dcd4824bc1d54a09741
Mforza <73272954+ByMarko99@users.noreply.github.com>
2023-12-05 12:09:29 +0100

    Revert "Crud"
    This reverts commit 90ea37eae6ddf3ee04762cc4292de456b5cc4cfe.

--- aula/resources/views/alumnos/index.blade.php
+++ aula/resources/views/alumnos/index.blade.php
@@ -6,19 +6,10 @@
 @section('content')
     <h1>IKASLE ZERRENDA</h1>
     <ol>
-        @foreach ($alumnos as $alumno)
-            <li> <a href="{{ route('alumnos.show', $alumno) }}"> {{ $alumno->nombre_apellido }} - {{ $alumno->edad }} </a>
-            </li>
-            <br>
-            <a href="{{ route('alumnos.edit', $alumno) }}"
-                style="color: #4A4A4A; text-decoration: none; background-color: #D9D9D9; padding: 10px 20px; border-radius: 5px; font-size: 14px;">Editatu</a>
-            <br>
-            <br>
-            <a href="{{ route('alumnos.delete', $alumno) }}"
-            style="color: #d42828; text-decoration: none; background-color: #D9D9D9; padding: 10px 20px; border-radius: 5px; font-size: 14px;">Ezabatu</a>
+    @foreach ($alumnos as $alumno)
+     <li> <a href="{{route('alumnos.show', $alumno)}}"> {{$alumno->nombre_apellido}} - {{$alumno->edad}} </a> </li>
+   <a href="{{route('alumnos.edit', $alumno)}}">Editatu</a>
 
-        @endforeach
+    @endforeach
     </ol>
-    <br>
-
 @endsection
